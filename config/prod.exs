11acb6263053addd0dda8835954e63c67a303ec0
Pranav Kumar Gitiri <informpranav@gmail.com>
2022-08-27 02:10:15 +0530

    change

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :calendlex, CalendlexWeb.Endpoint,
-  url: [scheme: "https", host: "peaceful-ravine-51542.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "desolate-plateau-70551.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
